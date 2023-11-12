26a09276aa872577872bf028064a7bf1d8adb66a
yeowon0411 <dudnjsdl11@naver.com>
2023-11-13 06:13:21 +0900

    push

--- src/Comp/Study/Study1.jsx
+++ src/Comp/Study/Study1.jsx
@@ -7,10 +7,10 @@ function Study1() {
 			<br />
 			<br />
 			<div className="article_font">
-				<a>머신러닝의 준지도학습에 대해서 공부하였다.</a><br />
-				<a>'일관성 정규화'방식에 대하여 공부를하고 발표를 하였다.</a><br />
+				<a>23-1에 DSG라는 데이터 분석 모임에 참여했었다</a><br />
+				<a>'일관성 정규화'방식에 대하여 공부를 하고 해당 내용에 대한 발표를 했다.</a><br />
 				<a>해당 방식에 쓰인 공식을 자세히 분석하여 전반적 과정을 알게 되었다.</a><br />
-				<a href="https://deep-cilantro-902.notion.site/Spring-2ab26b08de084256b8a19609aab91f87?pvs=4">머신러닝 발표자료를 보고싶다면?</a>
+				<a href="https://deep-cilantro-902.notion.site/73c5d80a41f749daa320bdacb77f4dca?pvs=4">머신러닝 발표자료를 보고싶다면?</a>
 			</div>
 		</div>
 	);
